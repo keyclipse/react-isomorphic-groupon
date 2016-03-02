3dc988c58bf2f581bbf63f019381f0ab87d2db37
Samuel Kitono <samuelkitono@skitono-personal-mac.group.on>
2016-03-02 18:23:37 +1100

    commit

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { IndexLink } from 'react-router';
-import { LinkContainer } from 'react-router-bootstrap';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
 import Helmet from 'react-helmet';
 import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
 import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
@@ -14,6 +9,10 @@ import config from '../../config';
 import { asyncConnect } from 'redux-async-connect';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import MyRawTheme from 'theme/material-ui.config';
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import { Link } from 'react-router';
 
 @asyncConnect([{
   promise: ({store: {dispatch, getState}}) => {
@@ -48,6 +47,11 @@ export default class App extends Component {
     muiTheme: React.PropTypes.object
   };
 
+  constructor() {
+    super();
+    this.handleLeftNavRouter = this.handleLeftNavRouter.bind(this);
+  }
+
   getChildContext() {
     return {
       muiTheme: ThemeManager.getMuiTheme(
@@ -70,68 +74,46 @@ export default class App extends Component {
     }
   }
 
+  handleLeftNavRouter(route) {
+    // Do DOM Diff refresh
+    this.props.pushState(route);
+  }
+
   handleLogout = (event) => {
     event.preventDefault();
     this.props.logout();
-  };
+  }
 
   render() {
-    const {user} = this.props;
+    // const {user} = this.props;
     const styles = require('./App.scss');
 
     return (
       <div className={styles.app}>
         <Helmet {...config.app.head}/>
-        <Navbar fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
-                <div className={styles.brand}/>
-                <span>{config.app.title}</span>
-              </IndexLink>
-            </Navbar.Brand>
-            <Navbar.Toggle/>
-          </Navbar.Header>
 
-          <Navbar.Collapse eventKey={0}>
-            <Nav navbar>
-              {user && <LinkContainer to="/chat">
-                <NavItem eventKey={1}>Chat</NavItem>
-              </LinkContainer>}
+        <LeftNav
+          ref="leftNav"
+          docked
+          open={false}>
+          <MenuItem
+            linkButton
+            containerElement={<Link to="/" />}
+            primaryText="Home"
+          />
+          <MenuItem
+            linkButton
+            containerElement={<Link to="/widgets" />}
+            primaryText="Widgets"
+          />
 
-              <LinkContainer to="/widgets">
-                <NavItem eventKey={2}>Widgets</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/survey">
-                <NavItem eventKey={3}>Survey</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/about">
-                <NavItem eventKey={4}>About Us</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/hello">
-                <NavItem eventKey={99}>Hello</NavItem>
-              </LinkContainer>
+        </LeftNav>
 
-              {!user &&
-              <LinkContainer to="/login">
-                <NavItem eventKey={5}>Login</NavItem>
-              </LinkContainer>}
-              {user &&
-              <LinkContainer to="/logout">
-                <NavItem eventKey={6} className="logout-link" onClick={this.handleLogout}>
-                  Logout
-                </NavItem>
-              </LinkContainer>}
-            </Nav>
-            {user &&
-            <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
-            <Nav navbar pullRight>
-              <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
-                <i className="fa fa-github"/>
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          style={{position: 'fixed'}}
+        />
 
         <div className={styles.appContent}>
           {this.props.children}
--- src/containers/Hello/Hello.js
+++ src/containers/Hello/Hello.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+/*
 import RaisedButton from 'material-ui/lib/raised-button';
 import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
+*/
 /*
 import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
 import {deepOrange500} from 'material-ui/lib/styles/colors';
 */
 
+/*
 const styles = {
   container: {
     textAlign: 'center',
     paddingTop: 200,
   },
 };
+*/
 
 /*
 const muiTheme = getMuiTheme({
@@ -33,7 +38,7 @@ export default class Hello extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      open: false
+      open: true
     };
     this.handleRequestClose = this.handleRequestClose.bind(this);
     this.handleTouchTap = this.handleTouchTap.bind(this);
@@ -52,6 +57,7 @@ export default class Hello extends Component {
   }
 
   render() {
+    /*
     const standardActions = (
       <FlatButton
         label="Okey"
@@ -59,23 +65,14 @@ export default class Hello extends Component {
         onTouchTap={this.handleRequestClose}
       />
     );
+    */
+
 
     return (
-      <div style={styles.container}>
-        <Dialog
-          open={this.state.open}
-          title="Super Secret Password"
-          actions={standardActions}
-          onRequestClose={this.handleRequestClose}
-        >
-          1-2-3-4-5
-        </Dialog>
-        <h1>material-ui</h1>
-        <h2>example project</h2>
-        <RaisedButton
-          label="Super Secret Password"
-          primary
-          onTouchTap={this.handleTouchTap}
+      <div>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
         />
       </div>
     );
